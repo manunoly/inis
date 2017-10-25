38d97dbcebfccb4e9336633c74e9000c99407432
Manuel Almaguer <manunoly@gmail.com>
2017-10-25 16:31:37 -0500

    add maps plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,27 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
-import { HttpModule } from '@angular/http';
+import { HttpModule } from "@angular/http";
 
 import { MyApp } from "./app.component";
 import { DriverRaceDetailPage } from "./../pages/driver-race-detail/driver-race-detail";
 import { ClientRaceDetailPage } from "./../pages/client-race-detail/client-race-detail";
 import { DataServiceProvider } from "../providers/data-service/data-service";
 
+//geolocalizacion
+import { Geolocation } from "@ionic-native/geolocation";
+import { GoogleMaps } from "@ionic-native/google-maps";
+
 @NgModule({
   declarations: [MyApp, DriverRaceDetailPage, ClientRaceDetailPage],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  imports: [BrowserModule, HttpModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, DriverRaceDetailPage, ClientRaceDetailPage],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    GoogleMaps,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     DataServiceProvider
   ]
--- src/pages/race-request/race-request.ts
+++ src/pages/race-request/race-request.ts
@@ -1,25 +1,75 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RaceRequestPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Geolocation, Geoposition } from "@ionic-native/geolocation";
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from "@ionic-native/google-maps";
 @IonicPage()
 @Component({
-  selector: 'page-race-request',
-  templateUrl: 'race-request.html',
+  selector: "page-race-request",
+  templateUrl: "race-request.html"
 })
 export class RaceRequestPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public geolocation: Geolocation,
+    public googleMaps: GoogleMaps
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RaceRequestPage');
+    this.getPosicion();
+  }
+  getPosicion():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
   }
 
+  loadMap(postion: Geoposition){
+    let latitude = postion.coords.latitude;
+    let longitud = postion.coords.longitude;
+    console.log(latitude, longitud);
+
+    // create a new map by passing HTMLElement
+    let element: HTMLElement = document.getElementById('map');
+
+    let map: GoogleMap = this.googleMaps.create(element);
+
+    // create LatLng object
+    let myPosition: LatLng = new LatLng(latitude,longitud);
+
+    // create CameraPosition
+    let position: CameraPosition<any> = {
+      target: myPosition,
+      zoom: 18,
+      tilt: 30
+    };
+
+    map.one(GoogleMapsEvent.MAP_READY).then(()=>{
+      console.log('Map is ready!');
+
+      // move the map's camera to position
+      map.moveCamera(position);
+
+      // create new marker
+      let markerOptions: MarkerOptions = {
+        position: myPosition,
+        title: 'Inicio Carrera'
+      };
+      map.addMarker(markerOptions);
+    }).catch(error=>{
+      console.log("Error con el marcado en google maps");
+      console.log(error);
+    });
+  }
 }
